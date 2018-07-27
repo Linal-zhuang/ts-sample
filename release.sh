88949523084b9fbf01fc6d9f66ea6afab91d8612
ilyydy <ilyydy@gmail.com>
2018-07-28 01:30:56 +0800

    fix

--- release.sh
+++ release.sh
@@ -5,14 +5,10 @@ CMD(){
     eval "$@"
 }
 
-CMD git checkout release &&\
-CMD git merge master &&\
 CMD node_modules/.bin/tsc &&\
 CMD npm test || exit 1
 
 CMD git add dist/*
 
 CMD git commit -m "'release'" &&\
-CMD git push origin release
-
-git checkout master
+CMD git push origin master
