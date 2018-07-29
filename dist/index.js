5059aed9919fbad21b67642e6710d109e0d58208
ilyydy <ilyydy@gmail.com>
2018-07-29 20:38:10 +0800

    release

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("source-map-support/register");
+const global_1 = __importDefault(require("./global"));
+console.log(`port is ${global_1.default.port}`);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSx1Q0FBb0M7QUFFcEMsc0RBQStCO0FBRS9CLE9BQU8sQ0FBQyxHQUFHLENBQUMsV0FBVyxnQkFBUSxDQUFDLElBQUksRUFBRSxDQUFDLENBQUEifQ==
